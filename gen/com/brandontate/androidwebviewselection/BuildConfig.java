7d780cc047c623e31148d49cd1e7d56a3a4538ba
srman <srman@y2books.com>
2014-10-17 10:59:39 +0900

    new add
    start

---
+++ gen/com/brandontate/androidwebviewselection/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.brandontate.androidwebviewselection;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
